fbec4a661bd7195ce553aabeafa6c42ae315cdd8
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-09-07 01:58:12 +0700

    Tasks

---
+++ Task_65/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.WriteLine("Введите первое положительное число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите последнее положительное число: ");
+int endNumber = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber(int firstNum, int endNum)
+{
+    Console.Write($"{firstNum} ");
+    if (firstNum > endNum)
+        NaturalNumber(firstNum - 1, endNum);
+    else if (firstNum < endNum)
+        NaturalNumber(firstNum + 1, endNum);
+}
+
+void PrintNaturalNumber(int firstNum, int endNum)
+{
+    if (firstNum == endNum)
+    {
+        Console.Write($"{firstNum}");
+        return;
+    }
+    Console.Write($"{firstNum}, ");
+
+    if (firstNum > endNum)
+        PrintNaturalNumber(firstNum - 1, endNum);
+    else if (firstNum < endNum)
+        PrintNaturalNumber(firstNum + 1, endNum);
+}
+
+PrintNaturalNumber(firstNumber, endNumber);
+Console.WriteLine();
---
+++ Task_69/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+int GetPower (int number, int p)
+{
+    if (p == 0)
+        return 1;
+    // if (p == 1)
+    //     return number;
+    
+    return number * GetPower (number, p - 1);
+}
+
+int pow = GetPower(a, b);
+Console.WriteLine($"{pow}");
